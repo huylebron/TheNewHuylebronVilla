a4042017852bbdc43d4cebb433d9e411203bc06c
huylebron <doquochuy27102003@gmail.com>
2025-02-04 13:47:52 +0700

    create villa number model

---
+++ NewHuylebronVilla.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewHuylebronVilla.Domain.Entities
+{
+    public class VillaNumber
+    {
+        [Key,
+          DatabaseGenerated(DatabaseGeneratedOption.None)] // ghi đè lên mặc định của EF core   để không tự tăng 
+        public int Villa_Number { get; set; }
+
+        [ForeignKey("Villa")] public int VillaId { get; set; }
+
+        //[ValidateNever]
+        public Villa Villa { get; set; }
+        public string? SpecialDetails { get; set; }
+    }
+}
