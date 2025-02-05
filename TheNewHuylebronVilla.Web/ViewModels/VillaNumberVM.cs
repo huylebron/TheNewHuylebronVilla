27c4b4c29bfde009b744b0a65c202033aad059dd
huylebron <doquochuy27102003@gmail.com>
2025-02-05 23:40:11 +0700

    create VillaNumber viewModel

---
+++ TheNewHuylebronVilla.Web/ViewModels/VillaNumberVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft . AspNetCore . Mvc . Rendering ;
+using NewHuylebronVilla . Domain . Entities ;
+
+namespace TheNewHuylebronVilla.Web.ViewModels ;
+
+public class VillaNumberVM
+{
+    public VillaNumber ? VillaNumber { get; set; }
+    public IEnumerable <SelectListItem> VillaList { get; set; }
+    
+}
