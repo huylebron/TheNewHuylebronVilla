8de860b016879ac6f79c39c9c5214d63a711f503
huylebron <doquochuy27102003@gmail.com>
2025-02-05 20:16:26 +0700

    add villaNumberController part 1

---
+++ TheNewHuylebronVilla.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NewHuylebronVilla.Domain.Entities;
+using NewHuylebronVilla.Infrastructure.Data;
+
+namespace TheNewHuylebronVilla.Web.Controllers
+
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewData["VillaId"] = list;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj)
+        {
+            ModelState.Remove("Villa");
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
+                return RedirectToAction("Index", "VillaNumber");
+            }
+
+            return View();
+        }
+    }
+}
