601f1bd3dbfd209ad1ad9277c7ce1134403933dc
huylebron <doquochuy27102003@gmail.com>
2025-02-05 20:14:24 +0700

    add không tự tăng

--- NewHuylebronVilla.Domain/Entities/VillaNumber.cs
+++ NewHuylebronVilla.Domain/Entities/VillaNumber.cs
@@ -5,13 +5,13 @@ namespace NewHuylebronVilla.Domain.Entities
 {
     public class VillaNumber
     {
-        [Key,
-          DatabaseGenerated(DatabaseGeneratedOption.None)] // ghi đè lên mặc định của EF core   để không tự tăng 
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Villa Number")]
         public int Villa_Number { get; set; }
 
         [ForeignKey("Villa")] public int VillaId { get; set; }
 
-        //[ValidateNever]
+        //    [ValidateNever]
         public Villa Villa { get; set; }
         public string? SpecialDetails { get; set; }
     }
