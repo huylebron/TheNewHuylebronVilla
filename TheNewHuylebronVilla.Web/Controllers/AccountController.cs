f3a67875a4b188363c1d6ef8dadcd9ff5f68bf56
huylebron <doquochuy27102003@gmail.com>
2025-02-09 16:30:16 +0700

    Add AccountController and ViewModels for user authentication

---
+++ TheNewHuylebronVilla.Web/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft . AspNetCore . Identity ;
+using Microsoft . AspNetCore . Mvc ;
+using NewHuylebronVilla . Application . Common . Interface ;
+using NewHuylebronVilla . Domain . Entities ;
+using TheNewHuylebronVilla . Web . ViewModels ;
+
+namespace TheNewHuylebronVilla.Web.Controllers ;
+
+public class AccountController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public AccountController(IUnitOfWork unitOfWork,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        SignInManager<ApplicationUser> signInManager)
+    {   
+        _roleManager = roleManager;
+        _unitOfWork = unitOfWork;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public IActionResult Login(string returnUrl = null)
+    {
+        
+        returnUrl??= Url.Content("~/");
+
+        LoginVM loginVM = new ()
+        {
+            RedirectUrl = returnUrl
+        };
+        return View(loginVM);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
