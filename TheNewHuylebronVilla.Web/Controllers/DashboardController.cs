4e390003eb7e90ce2d6b4c340da8ce0e9a873df2
huylebron <doquochuy27102003@gmail.com>
2025-02-11 01:13:36 +0700

    charts begin

--- TheNewHuylebronVilla.Web/Controllers/DashboardController.cs
+++ TheNewHuylebronVilla.Web/Controllers/DashboardController.cs
@@ -1,11 +1,52 @@
 ﻿using Microsoft . AspNetCore . Mvc ;
+using NewHuylebronVilla . Application . Common . Division ;
+using NewHuylebronVilla . Application . Common . Interface ;
+using TheNewHuylebronVilla . Web . ViewModels ;
 
 namespace TheNewHuylebronVilla.Web.Controllers ;
 
 public class DashboardController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
+    private readonly IUnitOfWork _unitOfWork ;
+    static int previousMonth = DateTime . Now . Month == 1 ? 12 : DateTime . Now . Month - 1 ;
+    readonly DateTime previousMonthStartDate = new ( DateTime . Now . Year , previousMonth , 1 ) ;
+    readonly DateTime currentMonthStartDate = new ( DateTime . Now . Year , DateTime . Now . Month , 1 ) ;
+
+    public DashboardController ( IUnitOfWork unitOfWork ) {
+        _unitOfWork = unitOfWork ;
+    }
+
+    public IActionResult Index ( ) {
+
+        return View ( ) ;
+    }
+
+    public async Task < IActionResult > GetTotalBookingRadialChartData ( ) {
+        var totalBookings = _unitOfWork . Booking . GetAll ( u => u . Status != Claim . StatusPending
+                                                                  || u . Status == Claim . StatusCancelled ) ;
+
+        var countByCurrentMonth = totalBookings . Count ( u => u . BookingDate >= currentMonthStartDate &&
+                                                               u . BookingDate <= DateTime . Now ) ;
+
+        var countByPreviousMonth = totalBookings . Count ( u => u . BookingDate >= previousMonthStartDate &&
+                                                                u . BookingDate <= currentMonthStartDate ) ;
+
+        RadialBarChartVM radialBarChartVM = new();
+
+
+        int increaseDecreaseRatio = 100;
+
+        if (countByPreviousMonth != 0)
+        {
+            increaseDecreaseRatio = Convert.ToInt32((countByCurrentMonth - countByPreviousMonth) / countByPreviousMonth * 100);
+        }
+
+        radialBarChartVM.TotalCount = totalBookings.Count();
+        radialBarChartVM.CountInCurrentMonth = countByCurrentMonth;
+        radialBarChartVM.HasRatioIncreased = currentMonthStartDate > previousMonthStartDate;
+        radialBarChartVM.Series = new int[] { increaseDecreaseRatio };
+
+        return Json(radialBarChartVM);
     }
 }
+
